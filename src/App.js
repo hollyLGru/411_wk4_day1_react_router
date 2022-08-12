1bc742a93393c45b99565cdc1cfedda82ec4a868
HollyLgru <H.grudovich@zollege.com>
2022-08-11 19:10:29 -0500

    HELP!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navigation from './components/Navigation'
 import Router from './Router'
-import Car from './components/Car'
 import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,32 +1,26 @@
+
 import React from 'react'
 import cars from '../cars.json'
 // import { Link } from 'react-router-dom'
-import { Routes, Route, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 // import MUI components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
-    const {carId} = useParams();
-    
-    function findCar() {
-        return cars.id === carId
-    }
-    
-    const carfilter = cars.find(findCar);
-    
-    console.log(carfilter)
+    const carId = useParams().id;
+    const findCar = cars.find((c) => parseInt(c.id) === carId);
 
+    //check out link below 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 
     return (
     <div>
-        {/* {carfilter.map((car, index) => {
-            return(
-                <h1>{car.Name}</h1>
-                )
-        })} */}
-
+        {findCar.map((car, index) => {
+            return (
+                <h1>{car.index} </h1> 
+            )
+            })}
     </div>
     )
 }
