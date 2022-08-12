454f98c5e2d95912a0b46f5e0a5b948ffbfb1411
HollyLgru <H.grudovich@zollege.com>
2022-08-11 19:45:31 -0500

    ALL DONE YEEYYYY

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,27 +1,32 @@
 
-import React from 'react'
-import cars from '../cars.json'
+import React from 'react';
+import cars from "../cars.json";
 // import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom';
+import { Container, Paper, Chip } from "@mui/material";
 
 // import MUI components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
     const carId = useParams().id;
-    const findCar = cars.find((c) => parseInt(c.id) === carId);
-
+    console.log(typeof carId)
+    const findCar = cars.find((car) => car.id === parseInt(carId));
+    console.log(typeof cars[0].id)
+    
+    console.log(findCar)
     //check out link below 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 
     return (
-    <div>
-        {findCar.map((car, index) => {
-            return (
-                <h1>{car.name} </h1> 
-            )
-            })}
-    </div>
+        <Container maxWidth="sm" className="car-container">
+        <Paper className="car-paper">
+          <h1>{findCar.Name}</h1>
+          {Object.keys(findCar).map((key, idx) => {
+            return <Chip key={idx} label={`${key}: ${findCar[key]}`}></Chip>
+          })}
+        </Paper>
+      </Container>
     )
 }
 
